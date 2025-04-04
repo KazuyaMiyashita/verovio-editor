65b34ccc6e15cfceecaf99b999efecaf08bfa919
Laurent Pugin <lxpugin@gmail.com>
2025-04-04 14:12:11 +0200

    dist cleanup

--- dist/verovio/verovio-app.js
+++
@@ -1,8 +0,0 @@
-import { App } from './app.js';
-export class VerovioApp extends App {
-    constructor(div, options) {
-        options.enableEditor = false;
-        super(div, options);
-    }
-}
-//# sourceMappingURL=verovio-app.js.map
