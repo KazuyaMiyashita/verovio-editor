b1dd8f7345966aad5a1ef3cf9a14e71d6fd3eb43
Laurent Pugin <lxpugin@gmail.com>
2025-09-18 19:03:12 +0200

    Hide menu (broken)

--- dist/toolbars/context-menu.js
+++ dist/toolbars/context-menu.js
@@ -10,12 +10,13 @@ export class ContextMenu extends GenericView {
         this.underlay = underlay;
         // One of the little quirks of writing in ES6, bind events
         this.eventManager = new EventManager(this);
+        this.eventManager.bind(this.div, 'click', this.hide);
         const fileMenu = appendDivTo(this.div, { class: `vrv-menu prout` });
         const fileMenuContent = appendDivTo(fileMenu, { class: `vrv-menu-content` });
         const fileImport = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `Import MEI file` });
         this.app.eventManager.bind(fileImport, 'click', this.app.fileImport);
         appendDivTo(fileMenuContent, { class: `vrv-v-separator` });
-        const helpAbout = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About this application` });
+        const helpAbout = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About the application` });
         const help2 = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About this application` });
         const fileRecentSubMenu = appendDivTo(fileMenuContent, { class: `vrv-submenu` });
         const fileRecent = appendDivTo(fileRecentSubMenu, { class: `vrv-submenu-text`, 'data-before': `Recent files` });
@@ -47,50 +48,9 @@ export class ContextMenu extends GenericView {
     onDismiss(e) {
         this.hide();
     }
-    updateToolbarGrp(grp, condition) {
-        if (grp === undefined) {
-            return;
-        }
-        if (condition)
-            grp.style.display = 'block';
-        else
-            grp.style.display = 'none';
-    }
-    updateToolbarBtnEnabled(btn, condition) {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition)
-            btn.classList.remove("disabled");
-        else
-            btn.classList.add("disabled");
-    }
-    updateToolbarBtnDisplay(btn, condition) {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition)
-            btn.style.display = 'block';
-        else
-            btn.style.display = 'none';
-    }
-    updateToolbarBtnToggled(btn, condition) {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition)
-            btn.classList.add("toggled");
-        else
-            btn.classList.remove("toggled");
-    }
-    updateToolbarSubmenuBtn(btn, condition) {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition)
-            btn.classList.add("vrv-menu-checked");
-        else
-            btn.classList.remove("vrv-menu-checked");
+    buildFor(id) {
+        //this.actionManager
+        this.app.rngLoader;
     }
 }
 //# sourceMappingURL=context-menu.js.map
--- ts/toolbars/context-menu.ts
+++ ts/toolbars/context-menu.ts
@@ -22,6 +22,7 @@ export class ContextMenu extends GenericView {
 
         // One of the little quirks of writing in ES6, bind events
         this.eventManager = new EventManager(this);
+        this.eventManager.bind(this.div, 'click', this.hide);
 
         const fileMenu = appendDivTo(this.div, { class: `vrv-menu prout` });
         const fileMenuContent = appendDivTo(fileMenu, { class: `vrv-menu-content` });
@@ -30,7 +31,7 @@ export class ContextMenu extends GenericView {
         this.app.eventManager.bind(fileImport, 'click', this.app.fileImport);
         appendDivTo(fileMenuContent, { class: `vrv-v-separator` });
 
-        const helpAbout = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About this application` });
+        const helpAbout = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About the application` });
 
         const help2 = appendDivTo(fileMenuContent, { class: `vrv-menu-text`, 'data-before': `About this application` });
 
@@ -75,43 +76,9 @@ export class ContextMenu extends GenericView {
         this.hide();
     }
 
-    protected updateToolbarGrp(grp: HTMLElement, condition: boolean): void {
-        if (grp === undefined) {
-            return;
-        }
-        if (condition) grp.style.display = 'block';
-        else grp.style.display = 'none';
+    buildFor(id: string): void {
+        //this.actionManager
+        this.app.rngLoader
     }
 
-    protected updateToolbarBtnEnabled(btn: HTMLElement, condition: boolean): void {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition) btn.classList.remove("disabled");
-        else btn.classList.add("disabled");
-    }
-
-    protected updateToolbarBtnDisplay(btn: HTMLElement, condition: boolean): void {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition) btn.style.display = 'block';
-        else btn.style.display = 'none';
-    }
-
-    protected updateToolbarBtnToggled(btn: HTMLElement, condition: boolean): void {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition) btn.classList.add("toggled");
-        else btn.classList.remove("toggled");
-    }
-
-    protected updateToolbarSubmenuBtn(btn: HTMLElement, condition: boolean): void {
-        if (btn === undefined) {
-            return;
-        }
-        if (condition) btn.classList.add("vrv-menu-checked");
-        else btn.classList.remove("vrv-menu-checked");
-    }
 }
