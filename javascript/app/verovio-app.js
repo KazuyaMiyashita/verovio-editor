bc5b97ae41a21c475ecbc013977975bd41e13b74
Laurent Pugin <lxpugin@gmail.com>
2026-01-27 11:05:13 +0100

    Remove pako from verovio-app

--- javascript/app/verovio-app.js
+++ javascript/app/verovio-app.js
@@ -13,12 +13,6 @@ loadScript( "https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/musi
   console.log( "Tone.js, Magenta.js, and HTML-MIDI-Player loaded" );
 } );
 
-// Load pako
-loadScript( "https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js", () =>
-{
-  console.log( "Pako loaded" );
-} );
-
 import { VerovioApp } from '../../dist/verovio-app.js';
 ( function ()
 {
